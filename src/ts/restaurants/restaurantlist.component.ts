061d2ac59446cb1b5af38fe806ddaf6a4fe8af49
Marcos Hernández <marcoslhc@gmail.com>
2016-08-28 10:35:34 -0400

    Update pipeline, add mock-ui

--- src/ts/restaurants/restaurantlist.component.ts
+++ src/ts/restaurants/restaurantlist.component.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Restaurant } from './restaurant.model';
+import { RestaurantsService } from './restaurants.service';
 
 @Component({
     selector: 'restaurant-list',
     template: `
         <table>
             <tr>
-                <th>Restauran Name</th>
+                <th>Restaurant Name</th>
             </tr>
             <tr *ngFor="let restaurant of restaurants">
                 <td>{{ restaurant.businessName }}</td>
             </tr>
         <table>
-    `
+    `,
+    providers: [RestaurantsService]
 })
-export class RestaurantListComponent {
+export class RestaurantListComponent implements OnInit {
     restaurants: Restaurant[] = [];
+    constructor(private RestaurantsService:RestaurantsService) {}
+    ngOnInit() {
+
+    }
 }
